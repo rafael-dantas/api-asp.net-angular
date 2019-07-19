326abb0a76a47aa05af331aca5b88e4702ebe18a
Rafael Dantas <53061135+rafael-dantas@users.noreply.github.com>
2019-07-18 22:33:48 -0300

    Add files via upload

---
+++ Rafael.LocalizaAmigos/Rafael.LocalizaAmigos.Api/Providers/SimpleAuthServerProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Rafael.LocalizaAmigos.Api.Providers
+{
+    public class SimpleAuthServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new string[] { "*" });
+            if (context.UserName != "amigo" || context.Password != "amigo")
+            {
+                context.SetError("invalid_user_or_password", "Usuário e/ou senha incorretos.");
+                return;
+            }
+            ClaimsIdentity identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            context.Validated(identity);
+        }
+    }
+}
---
+++ Rafael.LocalizaAmigos/Rafael.LocalizaAmigos.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Rafael.LocalizaAmigos.Api.Providers;
+
+[assembly: OwinStartup(typeof(Rafael.LocalizaAmigos.Api.Startup))]
+
+namespace Rafael.LocalizaAmigos.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromSeconds(50),
+                Provider = new SimpleAuthServerProvider()
+            };
+            app.UseOAuthAuthorizationServer(oAuthOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
